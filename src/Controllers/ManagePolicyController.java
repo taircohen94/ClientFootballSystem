bca018270c40463c92e305dbc1198ad76d204205
Tair Cohen <45098383+taircohen94@users.noreply.github.com>
2020-05-29 21:33:09 +0300

    Tair client (#8)
    * edit team property sending to server requests
    
    * edit team property finished - before checking
    
    * game id request for game manage
    
    * user authentication
    
    * Merge branch 'master' of https://github.com/taircohen94/ClientFootballSystem into TairClient
    
    # Conflicts:
    #	src/Client/Client.java
    
    * Merge branch 'master' of https://github.com/taircohen94/ClientFootballSystem into TairClient
    
    # Conflicts:
    #	src/Client/Client.java
    
    Co-authored-by: Alon <51872854+alongutm@users.noreply.github.com>

--- src/Controllers/ManagePolicyController.java
+++ src/Controllers/ManagePolicyController.java
@@ -4,20 +4,18 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 public class ManagePolicyController extends Controller {
 
     public RequiredField requiredField1;
     public RequiredField requiredField2;
-    public RequiredField requiredField3;
-    public RequiredField requiredField4;
-
     public ChoiceBox policyChoiceBox;
-
     public TextField leagueName;
     public TextField season;
-
+    public ComboBox cmbLeagueType;
+    public ComboBox cmbSeasonType;
 
     @FXML
     public void clickOnAssignGamePolicy(ActionEvent e) {
@@ -64,7 +62,36 @@ public class ManagePolicyController extends Controller {
 
 
     public void init() {
-    }
+        String ans = client.getAllLeagues();
+        String[] array;
+        if (ans != null) {
+            array = ans.split(",");
+            if (array[0].equals("Ok")) {
+                for (int i = 1; i < array.length; i++) {
+                    this.cmbLeagueType.getItems().add(array[i]);
+                }
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText(ans);
+                alert.showAndWait();
+            }
+        }
+
+        ans = client.getAllSeasons();
+        String[] array1;
+        if (ans != null) {
+            array1 = ans.split(",");
+            if (array1[0].equals("Ok")) {
+                for (int i = 1; i < array1.length; i++) {
+                    this.cmbSeasonType.getItems().add(array1[i]);
+                }
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText(ans);
+                alert.showAndWait();
+            }
+        }
+
 
 }
 
